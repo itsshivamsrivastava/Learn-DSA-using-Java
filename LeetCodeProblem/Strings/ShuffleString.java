65678f729af6d7f78c57d1053d7b205e1fb94f92
Shivam Srivastava <73056786+itsshivamsrivastava@users.noreply.github.com>
2022-10-03 21:17:04 +0530

    Changes in string folder

--- LeetCodeProblem/Strings/ShuffleString.java
+++ LeetCodeProblem/Strings/ShuffleString.java
@@ -3,8 +3,8 @@ package Strings;
 /*
     1528. Shuffle String
     You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
-    
     Return the shuffled string.
+    
      Input:
         j = 4 5 6 7 0 2 1 3
             c o d e l e e t
